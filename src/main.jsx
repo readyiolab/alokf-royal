d7416f28a96068ab3ff64dd4e0f344cef9497b8e
readyiolab <github@arbilo.com>
2025-12-26 00:10:36 +0530

    addedddd

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  createRoot(rootElement).render(<App />);
+}
